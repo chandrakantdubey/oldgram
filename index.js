56ffc96ddabb9a3469fbc5b6a8b125cf8ee47ae9
chandrakantdubey <919chandrakant@gmail.com>
2023-04-19 00:34:00 +0530

    got head scratching on the like counter functionality...funny I didn't had the js file linked to the page 😅🤣

--- index.js
+++ index.js
@@ -31,37 +31,42 @@ const posts = [
 
 const postContainer = document.getElementById("post-container");
 
-addPost();
+post();
 
-function addPost() {
-  for (let { user, index } of posts) {
+function post() {
+  for (let i = 0; i < posts.length; i++) {
+    let user = posts[i];
     const postElement = document.createElement("section");
     postElement.className = "post";
+
     postElement.innerHTML = `
     <div class="post-header">
       <img
-        src="./images/avatar-vangogh.jpg"
-        alt="avatar-vangogh"
+        src="./${user.avatar}"
+        alt="${user.name}"
         class="post-header-avatar"
       />
       <p class="post-header-username">
-        <span class="user-name"><strong>Vincent van Gogh</strong></span>
-        Zudert, Netherlands
+        <span class="user-name"><strong>${user.name}</strong></span>
+        ${user.location}
       </p>
     </div>
+
     <img
-      src="./images/post-vangogh.jpg"
-      alt="post-vangogh"
+      src="./${user.post}"
+      alt=${user.post}
       class="post-image"
     />
+
     <div class="post-reactions">
       <img
         src="./images/icon-heart.png"
         alt="heart"
         class="like post-reactions-icon"
+        id="${user.username}"
       />
       <img
-        src="./images//icon-comment.png"
+        src="./images/icon-comment.png"
         alt="comment"
         class="comment post-reactions-icon"
       />
@@ -71,58 +76,26 @@ function addPost() {
         class="share post-reactions-icon"
       />
     </div>
-    <div class="post-likes"><strong>21492 likes</strong></div>
+
+    <div class="post-likes">
+      <strong id="user-likes${user.username}">${user.likes}</strong> likes
+    </div>
+    
     <div class="post-comments">
-      <strong>vincey1853</strong>
-      just took a few mushrooms lol
-    </div>
-  </section>
-  <section class="post">
-    <div class="post-header">
-      <img
-        src="./images/avatar-vangogh.jpg"
-        alt="avatar-vangogh"
-        class="post-header-avatar"
-      />
-      <p class="post-header-username">
-        <span class="user-name"><strong>Vincent van Gogh</strong></span>
-        Zudert, Netherlands
-      </p>
-    </div>
-    <img
-      src="./images/post-vangogh.jpg"
-      alt="post-vangogh"
-      class="post-image"
-    />
-    <div class="post-reactions">
-      <img
-        src="./images/icon-heart.png"
-        alt="heart"
-        class="like post-reactions-icon"
-      />
-      <img
-        src="./images//icon-comment.png"
-        alt="comment"
-        class="comment post-reactions-icon"
-      />
-      <img
-        src="./images/icon-dm.png"
-        alt="share"
-        class="share post-reactions-icon"
-      />
-    </div>
-    <div class="post-likes"><strong>21492 likes</strong></div>
-    <div class="post-comments">
-      <strong>vincey1853</strong>
-      just took a few mushrooms lol
+        <strong>${user.username}</strong>
+        ${user.comment}
     </div>
     `;
-    const heartIcon = postElement.querySelector(".heart-icon");
-    heartIcon.addEventListener("click", () => {
-      const likesCount = postElement.querySelector(".likes-count .count");
-      const currentLikes = parseInt(likesCount.textContent);
-      likesCount.textContent = currentLikes + 1;
-    });
-    postContainer.appendChild(content);
+    postContainer.appendChild(postElement);
+    document
+      .querySelector(`#${user.username}`)
+      .addEventListener("click", () => {
+        const currentLikes = document.querySelector(
+          `#user-likes${user.username}`
+        ).textContent;
+
+        document.querySelector(`#user-likes${user.username}`).textContent =
+          +currentLikes + 1;
+      });
   }
 }
